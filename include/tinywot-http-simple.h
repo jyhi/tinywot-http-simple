83107ae49c7538ce9fbba192b19b31abd869dc5b
Junde Yhi <junde@yhi.moe>
2021-07-02 14:35:01 +0100

    tinywot-http-simple.h: add wrapper header
    This is the one indented for users to include.

---
+++ include/tinywot-http-simple.h
@@ -0,0 +1,12 @@
+/**
+ * \file tinywot-http-simple.h
+ * \brief Public API wrapper header.
+ *
+ * \copyright
+ * SPDX-FileCopyrightText: 2021 Junde Yhi <junde@yhi.moe>
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include "tinywot-http-simple/api.h"
